204db2ae6a17443fef3d6d487ba5027ce0aca238
Livingstone Okech <oklivinstone@gmail.com>
2024-10-31 08:26:51 -0700

    Project Database connection

---
+++ config/db.js
@@ -0,0 +1,21 @@
+// config/db.js
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
