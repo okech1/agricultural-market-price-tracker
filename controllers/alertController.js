4e14ed6ab72bb2c267cd2f23d34843a1016b4e2a
Livingstone Okech <oklivinstone@gmail.com>
2024-10-31 08:28:07 -0700

    Manages price alerts

---
+++ controllers/alertController.js
@@ -0,0 +1,11 @@
+// controllers/alertController.js
+
+const Alert = require('../models/Alert');
+
+exports.createAlert = async (req, res) => {
+  // Logic to create a new price alert
+};
+
+exports.getAlerts = async (req, res) => {
+  // Logic to get all alerts for a user
+};
