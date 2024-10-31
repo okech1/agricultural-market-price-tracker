d9f9e904a8cb1be9a3f3ef5444b6272720104b53
Livingstone Okech <oklivinstone@gmail.com>
2024-10-31 08:29:19 -0700

    Handles product price tracking logic

---
+++ controllers/priceController.js
@@ -0,0 +1,11 @@
+// controllers/priceController.js
+
+const Product = require('../models/Product');
+
+exports.getPrices = async (req, res) => {
+  // Logic to get product prices
+};
+
+exports.comparePrices = async (req, res) => {
+  // Logic to compare product prices across markets
+};
