23f0753e2d2c346e17180502a2c34bc67aa1d2d2
Livingstone Okech <oklivinstone@gmail.com>
2024-10-31 08:50:57 -0700

    product schema

---
+++ models/Product.js
@@ -0,0 +1,11 @@
+// models/Product.js
+
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  market: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
